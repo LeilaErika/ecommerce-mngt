54dd4a9fd632d356de6f42208a4376a1c13cf04c
LeilaErika <leilaellica@gmail.com>
2024-11-11 01:21:10 +0800

    we called and got all fixed, amen amin

--- index.js
+++ index.js
@@ -1,29 +1,3 @@
-// var express = require("express");
-// var bodyParser = require("body-parser");
-// var app = express();
-
-// const PORT = process.env.PORT || 5050;
-// var startPage = "index.html";
-
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-// app.use(express.static("./public"));
-
-// app.get("/", (req, res) => {
-//   res.sendFile(__dirname + "/public/" + startPage);
-// });
-
-// server = app.listen(PORT, function () {
-//   const address = server.address();
-//   const baseUrl = `http://${
-//     address.address == "::" ? "localhost" : address.address
-//   }:${address.port}`;
-//   console.log(`Demo project at: ${baseUrl}`);
-// });
-
-// module.exports = { app, server };
-
-
 // index.js
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -49,7 +23,9 @@ app.use("/api", clothingRoutes); // Register the routes under /api
 // Start the server
 const server = app.listen(PORT, function () {
   const address = server.address();
-  const baseUrl = `http://${address.address === "::" ? "localhost" : address.address}:${address.port}`;
+  const baseUrl = `http://${
+    address.address === "::" ? "localhost" : address.address
+  }:${address.port}`;
   console.log(`Demo project at: ${baseUrl}`);
 });
 
